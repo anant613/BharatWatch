20b0b8a2cc5a85d89cecce3fe821968856b3c991
Shushant321 <shushantupadhyay770@gmail.com>
2025-10-29 19:48:09 +0530

    Added/updated recommended videos, reels, images

---
+++ src/components/VideoCard/DummyVideos.js
@@ -0,0 +1,6 @@
+// src/components/VideoCard/DummyVideos.js
+export const demoVideos = [
+  { url: "/blog-1.png", title: "Sample 1", author: "Demo", views: "1K" },
+  { url: "/demoimg2.jpg", title: "Sample 2", author: "Demo", views: "1K" },
+  // aur images add karo
+];
