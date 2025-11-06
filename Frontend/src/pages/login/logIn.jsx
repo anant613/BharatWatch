79e110a3566207dfd02925f2a1033555f81bc324
anant613 <anantkapoor320@gmail.com>
2025-11-06 23:48:37 +0530

    ghnta nhi horha

--- Frontend/src/pages/login/logIn.jsx
+++ Frontend/src/pages/login/logIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Auth.css";
-import Navbar from "../../components/Navbar/navbar";
+import Navbar from "../../components/Navbar/navbar.jsx";
 
 const Login = () => {
   const navigate = useNavigate();
