8b2c60b92b70e809302081a94270d2b18a5b4d43
Siddhant <mishrasiddhant2605@gmail.com>
2025-11-16 23:32:45 +0530

    modified models

--- Backend/src/models/subscribe.model.js
+++ Backend/src/models/subscribe.model.js
@@ -1,21 +1,28 @@
 import mongoose from "mongoose";
 
-const subscriberSchema = new mongoose.Schema({
-    User:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    ChannelId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    channelName:{
-        type: String,
-        required: true
-    },
-   
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default  mongoose.model("Subscriber",subscriberSchema)
+// A user cannot subscribe to the same creator twice
+subscriptionSchema.index({ subscriber: 1, creator: 1 }, { unique: true });
+
+// Useful indexes for analytics
+subscriptionSchema.index({ creator: 1 }); // Get list of subscribers fast
+subscriptionSchema.index({ subscriber: 1 }); // Get user’s subscriptions fast
+
+export default mongoose.model("Subscription", subscriptionSchema);
