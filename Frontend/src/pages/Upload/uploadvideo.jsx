8c6e152994ac80336e8ad85ad8d1bb9776530dc2
anant613 <anantkapoor320@gmail.com>
2025-12-02 22:11:30 +0530

    wrote import for channel controllers and changed the ui of upload page

--- Frontend/src/pages/Upload/uploadvideo.jsx
+++ Frontend/src/pages/Upload/uploadvideo.jsx
@@ -44,14 +44,7 @@ const UploadVideo = () => {
 }, [editDraft, draftType]);
 
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    document.documentElement.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "";
-      document.documentElement.style.overflow = "";
-    };
-  }, []);
+
 
   const handleDrag = (e) => {
     e.preventDefault();
